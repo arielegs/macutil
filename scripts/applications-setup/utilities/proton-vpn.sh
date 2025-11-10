74e054ef0351ee04bd05177df2e3170e4f31592a
Arie Legs <79757505+arielegs@users.noreply.github.com>
2025-11-10 17:51:05 +0100

    fix: correct Proton VPN package name in installation script

--- scripts/applications-setup/utilities/proton-vpn.sh
+++ scripts/applications-setup/utilities/proton-vpn.sh
@@ -3,9 +3,9 @@
 . ../../common-script.sh
 
 installProtonVPN() {
-    if ! brewprogram_exists proton-vpn; then
+    if ! brewprogram_exists protonvpn; then
         printf "%b\n" "${YELLOW}Installing Proton VPN...${RC}"
-        brew install --cask proton-vpn
+        brew install --cask protonvpn
         if [ $? -ne 0 ]; then
             printf "%b\n" "${RED}Failed to install Proton VPN. Please check your Homebrew installation or try again later.${RC}"
             exit 1
