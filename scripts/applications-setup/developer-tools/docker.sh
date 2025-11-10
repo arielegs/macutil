8ae992a4503d807b3b0d274f95e5daa5b0e0706b
Arie Legs <79757505+arielegs@users.noreply.github.com>
2025-11-10 18:03:45 +0100

    feat: enhance Docker installation process with background execution and spinner

--- scripts/applications-setup/developer-tools/docker.sh
+++ scripts/applications-setup/developer-tools/docker.sh
@@ -5,7 +5,21 @@
 installDocker() {
     if ! brewprogram_exists docker; then
         printf "%b\n" "${YELLOW}Installing Docker...${RC}"
-        brew install --cask docker > /dev/null 2>&1
+        
+        # Run brew install in the background and show a spinner
+        brew install --cask docker > /dev/null 2>&1 &
+        pid=$!
+        spin='-\|/'
+        i=0
+        while kill -0 $pid 2>/dev/null
+        do
+            i=$(( (i+1) % 4 ))
+            printf "\r${spin:$i:1}"
+            sleep .1
+        done
+        printf "\r"
+
+        wait $pid
         if [ $? -ne 0 ]; then
             printf "%b\n" "${RED}Failed to install Docker. Please check your Homebrew installation or try again later.${RC}"
             exit 1
